a672d991185fbc2572456d57b5f776f7f7505e67
Kyle MacDonald <macdonaldky@gmail.com>
2018-02-25 14:53:24 -0500

    stub in inquirer.config.js

---
+++ inquirer.config.js
@@ -0,0 +1,12 @@
+var inquirer = require("inquirer");
+var questions = [
+  {
+    type: "input",
+    name: "first_name",
+    message: "What's your first name?"
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log("i have received your responses", answers);
+});
